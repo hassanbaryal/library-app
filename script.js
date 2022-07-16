d8ad00c3c0cce77cf1b6db944c98886dab48fe1b
hassanbaryal <h.baryal2@gmail.com>
2022-07-16 18:25:05 -0400

    Created function constructor for Book

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 
 
+function Book(name, author, readStatus, link, pageCount, rating, description) {
+    this.name = name
+    this.author = author
+    this.readStatus = readStatus
+    this.link = link
+    this.pageCount = pageCount
+    this.rating = rating
+    this.description = description
+}
+
+let library = []
+
 const addBookBtn = document.getElementById("add-book-btn")
 const bookForm = document.getElementById("book-form")
 const bookFormCloseBtn = document.getElementById("close-new-book")
